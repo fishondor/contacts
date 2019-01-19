201d2cccd7b2f60c39445f571a201e2a864c87e5
Ariel Fisher <fishondor@gmail.com>
2019-01-19 11:31:06 +0200

    Create constants files for images,  strings and styles

---
+++ src/constants/images.js
@@ -0,0 +1,15 @@
+import USER_PLACEHODER from '../assets/person.svg';
+import PROFESSIONAL_DRIVER_ICON from '../assets/professional.svg';
+import CITIZEN_DRIVER_ICON from '../assets/citizen.svg';
+import SEARCH_ICON from '../assets/search.svg';
+
+const DRIVER_ICONS = {
+    professional: PROFESSIONAL_DRIVER_ICON,
+    citizen: CITIZEN_DRIVER_ICON
+}
+
+export {
+    USER_PLACEHODER,
+    DRIVER_ICONS,
+    SEARCH_ICON
+}
---
+++ src/constants/strings.js
@@ -0,0 +1,20 @@
+const APP_TITLE = "Contact List";
+const CONTACT_CARD = {
+    RANK: 'Rank',
+    PHONE: 'Phone Number',
+    EMAIL: 'Email'
+}
+
+const NAVBAR = {
+    TITLE: APP_TITLE
+}
+
+const SEARCH = {
+    HINT: 'Search...'
+}
+
+export {
+    CONTACT_CARD,
+    NAVBAR,
+    SEARCH
+}
